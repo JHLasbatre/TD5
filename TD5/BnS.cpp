1551c83a2519eeb7119d1f2ceff91746f9fb5f0b
JHLasbatre <jh.lasbatre@outlook.fr>
2017-11-03 10:46:34 +0100

    maj

--- TD5/BnS.cpp
+++ TD5/BnS.cpp
@@ -12,5 +12,6 @@ BnS::BnS(Option o, double _t)
 	double S = o.getS();
 	double b = o.getb();
 	t = _t;
-	d1 = (ln(S/K) + (r+1/2 * sigma* sigma)*(T-t))
+	d1 = (log(S / K) + (r + 1 / 2 * sigma* sigma)*(T - t))/(sigma* pow(T - t, 0.5));
+	d2 = d1 - sigma* pow(T - t, 0.5);
 }
--- TD5/Source.cpp
+++ TD5/Source.cpp
@@ -3,6 +3,11 @@
 #include "Header.h"
 using namespace std;
 
+
+void test()
+{
+	Option o = Option(0.025, 10, 25, )
+}
 int main()
 {
 
