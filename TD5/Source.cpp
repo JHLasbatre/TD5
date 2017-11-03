7b96bf10929d0eb1ad353923eaedb0ccd895245d
JHLasbatre <jh.lasbatre@outlook.fr>
2017-11-03 11:15:28 +0100

    maj

--- TD5/Source.cpp
+++ TD5/Source.cpp
@@ -6,10 +6,11 @@ using namespace std;
 
 void test()
 {
-	Option o = Option(0.025, 10, 25, )
+	Option o = Option(0.025, 10, 25, 2, 21, 0.5);
+	o.print();
 }
 int main()
 {
-
+	test();
 	return 0;
 }
